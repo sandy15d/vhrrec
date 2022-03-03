03a0f1f66191b01e571bdd714c036c443b3b86b9
Sandeep Dewangan <sandeepdewangan2012@gmail.com>
2022-03-03 12:14:41 +0530

    aaaa

--- resources/views/common/candidate_detail.blade.php
+++ resources/views/common/candidate_detail.blade.php
@@ -3581,15 +3581,15 @@ if ($OfBasic != null && $OfBasic->Grade != null) {
                     </button>
                 </div>
                 <div class="modal-body">
-                    @if ($Rec->Resume->extension() == 'pdf')
+                  {{-- //  @if ($Rec->Resume->extension() == 'pdf') --}}
                         <object width="760" height="500" data="{{ URL::to('/') }}/uploads/Resume/{{ $Rec->Resume }}"
                             id="{{ $Rec->JCId }}"></object>
-                    @else
+                   {{--  @else
                         <iframe src="//{{ URL::to('/') }}/uploads/Resume/{{ $Rec->Resume }}" width="668" height="714"
                             frameborder="0" marginwidth="0" marginheight="0" scrolling="no"
                             style="border:1px solid #CCC; border-width:1px; margin-bottom:5px; max-width: 100%;"
                             allowfullscreen> </iframe>
-                    @endif
+                    @endif --}}
 
 
                 </div>
