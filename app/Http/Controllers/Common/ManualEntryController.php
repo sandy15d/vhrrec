66fb0c2c713b81931cc306ff502f062b0da6070a
Sandeep Dewangan <sandeepdewangan2012@gmail.com>
2022-03-08 11:17:42 +0530

    change order of rec manual entry list

--- app/Http/Controllers/Common/ManualEntryController.php
+++ app/Http/Controllers/Common/ManualEntryController.php
@@ -87,7 +87,7 @@ class ManualEntryController extends Controller
         }
 
         $mrf = $usersQuery->select('manpowerrequisition.MRFId', 'manpowerrequisition.Type', 'manpowerrequisition.JobCode', 'manpowerrequisition.CreatedBy', 'master_designation.DesigName', 'manpowerrequisition.Status', 'manpowerrequisition.CreatedTime')
-            ->Join('master_designation', 'manpowerrequisition.DesigId', '=', 'master_designation.DesigId', 'left');
+            ->Join('master_designation', 'manpowerrequisition.DesigId', '=', 'master_designation.DesigId', 'left')->orderBy('manpowerrequisition.CreatedTime', 'desc');
 
         return datatables()->of($mrf)
             ->addIndexColumn()
