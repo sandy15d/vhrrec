558b508b10c17a54ed2544091cce779a9f587a73
Sandeep Dewangan <sandeepdewangan2012@gmail.com>
2022-03-21 17:45:47 +0530

    aaaa

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -135,7 +135,7 @@ $upcommingJoining = DB::table('candjoining')
 
         <div class="row row-cols-1 row-cols-md-2 row-cols-xl-4">
             <div class="col">
-                <a href="/TechnicalScreening">
+                <a href="/job_applications">
                     <div class="card radius-10">
                         <div class="card-body">
                             <div class="d-flex align-items-center">
