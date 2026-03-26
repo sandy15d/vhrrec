7ff4cb2a8a40cc9796a1eb3f8151cb281aa9899e
Sandeep Dewangan <sandeepdewangan2012@gmail.com>
2026-03-26 17:13:00 +0530

    Update JobApplicationController.php

--- app/Http/Controllers/Common/JobApplicationController.php
+++ app/Http/Controllers/Common/JobApplicationController.php
@@ -1989,7 +1989,7 @@ class JobApplicationController extends Controller
         // Add additional documents if the user is Professional
         if ($Professional == 'P') {
             $documents = array_merge($documents, [
-                'PFeNomination' => 'PF Nomination',
+              /*   'PFeNomination' => 'PF Nomination', */
                 'Form16' => 'Form 16',
                 'Resignation' => 'Self Resignation Declaration',
                 'Resignation_Accept' => 'Resignation Acceptance by previous employer'
